e322e2b3dc602023df048bf8ab03f17128db7ce0
muneerpala2 <muneerpala2@gmail.com>
2025-03-13 13:38:34 -0400

    Deleted example-app

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { ASWebAuthSession } from '@palainteractive/capacitor-plugin-ios-webauth-session';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    ASWebAuthSession.echo({ value: inputValue })
-}
